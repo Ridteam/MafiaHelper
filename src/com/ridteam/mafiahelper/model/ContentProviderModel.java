eb86deda26d2674dace130b8b6e7a0d634bde361
Inna N <foenix.i@gmail.com>
2013-12-19 00:57:22 +0400

    Added 'delete' in content provider

--- src/com/ridteam/mafiahelper/model/ContentProviderModel.java
+++ src/com/ridteam/mafiahelper/model/ContentProviderModel.java
@@ -1,12 +1,19 @@
 package com.ridteam.mafiahelper.model;
 
+import com.ridteam.mafiahelper.database.MafiaHelperTables.PlayerEffectsColumns;
+import com.ridteam.mafiahelper.database.MafiaHelperTables.PlayerEffectsTable;
+import com.ridteam.mafiahelper.database.MafiaHelperTables.PlayerHistoryTable;
 import com.ridteam.mafiahelper.database.MafiaHelperTables.PlayersTable;
+import com.ridteam.mafiahelper.database.MafiaHelperTables.RolePropertiesTable;
+import com.ridteam.mafiahelper.database.MafiaHelperTables.RolesTable;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.content.CursorLoader;
 import android.util.Log;
+
 /**
  * 
  * @author Foenix Wrapper class for database actorlive.db queries
@@ -14,99 +21,101 @@ import android.util.Log;
 
 public class ContentProviderModel implements IModel {
 	private static final String TAG = "ContentProviderModel";
-	private Context mContext; 
-	
+	private Context mContext;
+
 	public ContentProviderModel(Context context) {
 		super();
-		
+
 		this.mContext = context;
 	}
-	
+
 	@Override
 	public Cursor getPlayers() {
-		Log.d(TAG,PlayersTable.CONTENT_URI.toString() );
+		Log.d(TAG, PlayersTable.CONTENT_URI.toString());
 		return mContext.getContentResolver().query(PlayersTable.CONTENT_URI,
 				null, null, null, null);
 
 	}
-	
+
 	public CursorLoader getLoaderPlayers() {
-		Log.d(TAG,PlayersTable.CONTENT_URI.toString() );
-		return new CursorLoader(mContext, PlayersTable.CONTENT_URI,
-		        null, null, null, null);
+		Log.d(TAG, PlayersTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, PlayersTable.CONTENT_URI, null, null,
+				null, null);
 	}
 
 	@Override
 	public void addPlayer(String name) {
 		ContentValues contentValues = new ContentValues();
 		contentValues.put(PlayersTable.NAME, name);
-		mContext.getContentResolver().insert(PlayersTable.CONTENT_URI, contentValues);
+		mContext.getContentResolver().insert(PlayersTable.CONTENT_URI,
+				contentValues);
 	}
 
 	@Override
 	public void removePlayer(long playerId) {
-		// TODO Auto-generated method stub
-		
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI,
+				String.valueOf(playerId));
+		mContext.getContentResolver().delete(uri, null, null);
 	}
 
 	@Override
 	public void setRole(long playerId, long roleId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setLive(long playerId, boolean isLive) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setMarkAsDead(long playerId, boolean isMarkAsDead) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void killMarkAsDead() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setNomination(long playerId, boolean isNominant) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void setNomination(int minimumAccuse) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resetNomination() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void increaseAccuse(long playerId, int value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resetAccuse() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void increaseRebuke(long playerId, int value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -118,25 +127,32 @@ public class ContentProviderModel implements IModel {
 	@Override
 	public void addPlayersEffects(long playerId, int type, int time) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removePlayersEffects(long playerId, int type) {
-		// TODO Auto-generated method stub
-		
+		mContext.getContentResolver()
+				.delete(PlayerEffectsTable.CONTENT_URI,
+						PlayerEffectsColumns.PLAYER_ID + "=?"
+								+ PlayerEffectsColumns.TYPE + "= ?",
+						new String[] { String.valueOf(playerId),
+								String.valueOf(type) });
+
 	}
 
 	@Override
 	public void removePlayersEffects(long playerId) {
-		// TODO Auto-generated method stub
-		
+		mContext.getContentResolver().delete(PlayerEffectsTable.CONTENT_URI,
+				PlayerEffectsColumns.PLAYER_ID + "=?",
+				new String[] { String.valueOf(playerId) });
+
 	}
 
 	@Override
 	public void decreasePlayersEffectsTime(int value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -154,13 +170,15 @@ public class ContentProviderModel implements IModel {
 	@Override
 	public void addPlayersHistory(String name, String date, boolean isWin) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removePlayersHistory(long playerId) {
-		// TODO Auto-generated method stub
-		
+		mContext.getContentResolver().delete(PlayerHistoryTable.CONTENT_URI,
+				PlayerHistoryTable.PLAYER_ID + "=?",
+				new String[] { String.valueOf(playerId) });
+
 	}
 
 	@Override
@@ -178,20 +196,21 @@ public class ContentProviderModel implements IModel {
 	@Override
 	public void addRole(String name, String desc, int side, String picture) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void editRole(long roleId, String name, String desc, int side,
 			String picture) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removeRole(long roleId) {
-		// TODO Auto-generated method stub
-		
+		Uri uri = Uri.withAppendedPath(RolesTable.CONTENT_URI,
+				String.valueOf(roleId));
+		mContext.getContentResolver().delete(uri, null, null);
 	}
 
 	@Override
@@ -203,16 +222,15 @@ public class ContentProviderModel implements IModel {
 	@Override
 	public void addRoleProperty(long roleId, int type, int value) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void removeRolePropertysByRole(long roleId) {
-		// TODO Auto-generated method stub
-		
+		mContext.getContentResolver().delete(RolePropertiesTable.CONTENT_URI,
+				RolePropertiesTable.ROLE_ID + "=?",
+				new String[] { String.valueOf(roleId) });
+
 	}
-
-
-
 
 }
