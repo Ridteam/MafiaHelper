3bbbcdf0cfbc44223c7a6f42bc985e961d70e506
Inna N <foenix.i@gmail.com>
2013-12-24 18:52:32 +0400

    added ContentProvider 100500 methods

--- src/com/ridteam/mafiahelper/database/MafiaHelperProvider.java
+++ src/com/ridteam/mafiahelper/database/MafiaHelperProvider.java
@@ -24,45 +24,42 @@ import android.util.Log;
 public class MafiaHelperProvider extends ContentProvider {
 
 	private static final String TAG = "MafiaHelperProvider";
-    private static final boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
+	private static final boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
 
-    private MafiaHelperDBHelper mOpenHelper;
+	private MafiaHelperDBHelper mOpenHelper;
 
-    private static final UriMatcher sUriMatcher = buildUriMatcher();
+	private static final UriMatcher sUriMatcher = buildUriMatcher();
 
-    private static final int PLAYERS = 100;
-    private static final int PLAYERS_ID = 101;
-    
-    private static final int PLAYER_EFFECTS = 200;
-    private static final int PLAYER_EFFECTS_ID = 201;
-    
-    
-    private static final int PLAYER_HISTORY = 300;
-    private static final int PLAYER_HISTORY_ID = 301;
-    
-    private static final int ROLES = 400;
-    private static final int ROLES_ID = 401;
-    
-    private static final int ROLE_PROPERTIES = 500; 
-    private static final int ROLE_PROPERTIES_ID = 501;
-    
+	private static final int PLAYERS = 100;
+	private static final int PLAYERS_ID = 101;
+
+	private static final int PLAYER_EFFECTS = 200;
+	private static final int PLAYER_EFFECTS_ID = 201;
+
+	private static final int PLAYER_HISTORY = 300;
+	private static final int PLAYER_HISTORY_ID = 301;
+
+	private static final int ROLES = 400;
+	private static final int ROLES_ID = 401;
+
+	private static final int ROLE_PROPERTIES = 500;
+	private static final int ROLE_PROPERTIES_ID = 501;
+
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 		int rowsAffected = 0;
 		int id;
-		Log.d(TAG,"delete Uri = "+uri);
+		Log.d(TAG, "delete Uri = " + uri);
 		switch (sUriMatcher.match(uri)) {
 		case PLAYERS_ID:
 			id = Integer.parseInt(uri.getLastPathSegment());
 			if (TextUtils.isEmpty(selection)) {
-	            rowsAffected = db.delete(Tables.PLAYERS,
-	                    BaseColumns._ID + "=" + id, null);
-	        } else {
-	            rowsAffected = db.delete(Tables.PLAYERS,
-	                    selection + " and " + BaseColumns._ID + "=" + id,
-	                    selectionArgs);
-	        }
+				rowsAffected = db.delete(Tables.PLAYERS, BaseColumns._ID + "=" + id, null);
+			} else {
+				rowsAffected = db.delete(Tables.PLAYERS, selection + " and " + BaseColumns._ID + "=" + id,
+						selectionArgs);
+			}
 			break;
 		case PLAYERS:
 			rowsAffected = db.delete(Tables.PLAYERS, selection, selectionArgs);
@@ -70,52 +67,43 @@ public class MafiaHelperProvider extends ContentProvider {
 		case PLAYER_EFFECTS_ID:
 			id = Integer.parseInt(uri.getLastPathSegment());
 			if (TextUtils.isEmpty(selection)) {
-	            rowsAffected = db.delete(Tables.PLAYER_EFFECTS,
-	                    BaseColumns._ID + "=" + id, null);
-	        } else {
-	            rowsAffected = db.delete(Tables.PLAYER_EFFECTS,
-	                    selection + " and " + BaseColumns._ID + "=" + id,
-	                    selectionArgs);
-	        }
+				rowsAffected = db.delete(Tables.PLAYER_EFFECTS, BaseColumns._ID + "=" + id, null);
+			} else {
+				rowsAffected = db.delete(Tables.PLAYER_EFFECTS, selection + " and " + BaseColumns._ID + "=" + id,
+						selectionArgs);
+			}
 			break;
 		case PLAYER_EFFECTS:
 			rowsAffected = db.delete(Tables.PLAYERS, selection, selectionArgs);
 		case PLAYER_HISTORY_ID:
 			id = Integer.parseInt(uri.getLastPathSegment());
 			if (TextUtils.isEmpty(selection)) {
-	            rowsAffected = db.delete(Tables.PLAYER_HISTORY,
-	                    BaseColumns._ID + "=" + id, null);
-	        } else {
-	            rowsAffected = db.delete(Tables.PLAYER_HISTORY,
-	                    selection + " and " + BaseColumns._ID + "=" + id,
-	                    selectionArgs);
-	        }
+				rowsAffected = db.delete(Tables.PLAYER_HISTORY, BaseColumns._ID + "=" + id, null);
+			} else {
+				rowsAffected = db.delete(Tables.PLAYER_HISTORY, selection + " and " + BaseColumns._ID + "=" + id,
+						selectionArgs);
+			}
 			break;
 		case PLAYER_HISTORY:
 			rowsAffected = db.delete(Tables.PLAYER_HISTORY, selection, selectionArgs);
 		case ROLES_ID:
 			id = Integer.parseInt(uri.getLastPathSegment());
 			if (TextUtils.isEmpty(selection)) {
-	            rowsAffected = db.delete(Tables.ROLES,
-	                    BaseColumns._ID + "=" + id, null);
-	        } else {
-	            rowsAffected = db.delete(Tables.ROLES,
-	                    selection + " and " + BaseColumns._ID + "=" + id,
-	                    selectionArgs);
-	        }
+				rowsAffected = db.delete(Tables.ROLES, BaseColumns._ID + "=" + id, null);
+			} else {
+				rowsAffected = db.delete(Tables.ROLES, selection + " and " + BaseColumns._ID + "=" + id, selectionArgs);
+			}
 			break;
 		case ROLES:
 			rowsAffected = db.delete(Tables.ROLES, selection, selectionArgs);
 		case ROLE_PROPERTIES_ID:
 			id = Integer.parseInt(uri.getLastPathSegment());
 			if (TextUtils.isEmpty(selection)) {
-	            rowsAffected = db.delete(Tables.ROLE_PROPERTIES,
-	                    BaseColumns._ID + "=" + id, null);
-	        } else {
-	            rowsAffected = db.delete(Tables.ROLE_PROPERTIES,
-	                    selection + " and " + BaseColumns._ID + "=" + id,
-	                    selectionArgs);
-	        }
+				rowsAffected = db.delete(Tables.ROLE_PROPERTIES, BaseColumns._ID + "=" + id, null);
+			} else {
+				rowsAffected = db.delete(Tables.ROLE_PROPERTIES, selection + " and " + BaseColumns._ID + "=" + id,
+						selectionArgs);
+			}
 			break;
 		case ROLE_PROPERTIES:
 			rowsAffected = db.delete(Tables.ROLE_PROPERTIES, selection, selectionArgs);
@@ -130,30 +118,30 @@ public class MafiaHelperProvider extends ContentProvider {
 	@Override
 	public String getType(Uri uri) {
 		final int match = sUriMatcher.match(uri);
-        switch (match) {
-            case ROLES:
-                return RolesTable.CONTENT_TYPE;
-            case ROLES_ID:
-                return RolesTable.CONTENT_ITEM_TYPE;
-            case ROLE_PROPERTIES:
-                return RolePropertiesTable.CONTENT_TYPE;
-            case ROLE_PROPERTIES_ID:
-                return RolePropertiesTable.CONTENT_ITEM_TYPE;
-            case PLAYERS:
-                return PlayersTable.CONTENT_TYPE;
-            case PLAYERS_ID:
-                return PlayersTable.CONTENT_ITEM_TYPE;
-            case PLAYER_EFFECTS:
-                return PlayerEffectsTable.CONTENT_TYPE;
-            case PLAYER_EFFECTS_ID:
-                return PlayerEffectsTable.CONTENT_ITEM_TYPE;
-            case PLAYER_HISTORY:
-                return PlayerHistoryTable.CONTENT_TYPE;
-            case PLAYER_HISTORY_ID:
-                return PlayerHistoryTable.CONTENT_ITEM_TYPE;
-            default:
-                throw new UnsupportedOperationException("Unknown uri: " + uri);
-        } 
+		switch (match) {
+		case ROLES:
+			return RolesTable.CONTENT_TYPE;
+		case ROLES_ID:
+			return RolesTable.CONTENT_ITEM_TYPE;
+		case ROLE_PROPERTIES:
+			return RolePropertiesTable.CONTENT_TYPE;
+		case ROLE_PROPERTIES_ID:
+			return RolePropertiesTable.CONTENT_ITEM_TYPE;
+		case PLAYERS:
+			return PlayersTable.CONTENT_TYPE;
+		case PLAYERS_ID:
+			return PlayersTable.CONTENT_ITEM_TYPE;
+		case PLAYER_EFFECTS:
+			return PlayerEffectsTable.CONTENT_TYPE;
+		case PLAYER_EFFECTS_ID:
+			return PlayerEffectsTable.CONTENT_ITEM_TYPE;
+		case PLAYER_HISTORY:
+			return PlayerHistoryTable.CONTENT_TYPE;
+		case PLAYER_HISTORY_ID:
+			return PlayerHistoryTable.CONTENT_ITEM_TYPE;
+		default:
+			throw new UnsupportedOperationException("Unknown uri: " + uri);
+		}
 	}
 
 	@Override
@@ -192,90 +180,78 @@ public class MafiaHelperProvider extends ContentProvider {
 	@Override
 	public boolean onCreate() {
 		final Context context = getContext();
-		Log.d(TAG, "onCreate()**********");
-        mOpenHelper = new MafiaHelperDBHelper(context);
-        return true; 
+		mOpenHelper = new MafiaHelperDBHelper(context);
+		return true;
 	}
 
 	@Override
-	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-            String sortOrder) {
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		Cursor cursor = null;
 		String ssid;
 		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 		SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-		
+
 		switch (sUriMatcher.match(uri)) {
 		case PLAYERS:
-			Log.d(TAG,"PLAYERS");
-			/*qb.setTables(Tables.PLAYERS);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);*/
+			Log.d(TAG, "PLAYERS");
+			qb.setTables(Tables.PLAYERS);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			cursor = PlayersTable.getPlayersWithRoles(db);
 			break;
 		case PLAYERS_ID:
-			Log.d(TAG,"PLAYERS_ID");
+			Log.d(TAG, "PLAYERS_ID");
 			qb.setTables(Tables.PLAYERS);
 			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
 			qb.appendWhere(BaseColumns._ID + "=" + ssid);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case PLAYER_EFFECTS:
-			Log.d(TAG,"PLAYER_EFFECTS");
+			Log.d(TAG, "PLAYER_EFFECTS");
 			qb.setTables(Tables.PLAYER_EFFECTS);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case PLAYER_EFFECTS_ID:
-			Log.d(TAG,"PLAYER_EFFECTS_ID");
+			Log.d(TAG, "PLAYER_EFFECTS_ID");
 			qb.setTables(Tables.PLAYER_EFFECTS);
 			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
 			qb.appendWhere(BaseColumns._ID + "=" + ssid);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);			
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case PLAYER_HISTORY:
-			Log.d(TAG,"PLAYER_HISTORY");
+			Log.d(TAG, "PLAYER_HISTORY");
 			qb.setTables(Tables.PLAYER_HISTORY);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case PLAYER_HISTORY_ID:
-			Log.d(TAG,"PLAYER_HISTORY_ID");
+			Log.d(TAG, "PLAYER_HISTORY_ID");
 			qb.setTables(Tables.PLAYER_HISTORY);
 			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
 			qb.appendWhere(BaseColumns._ID + "=" + ssid);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);			
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case ROLES:
-			Log.d(TAG,"ROLES");
+			Log.d(TAG, "ROLES");
 			qb.setTables(Tables.ROLES);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case ROLES_ID:
-			Log.d(TAG,"ROLES_ID");
+			Log.d(TAG, "ROLES_ID");
 			qb.setTables(Tables.ROLES);
 			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
 			qb.appendWhere(BaseColumns._ID + "=" + ssid);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);			
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case ROLE_PROPERTIES:
-			Log.d(TAG,"ROLE_PROPERTIES");
+			Log.d(TAG, "ROLE_PROPERTIES");
 			qb.setTables(Tables.ROLE_PROPERTIES);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		case ROLE_PROPERTIES_ID:
-			Log.d(TAG,"ROLE_PROPERTIES_ID");
+			Log.d(TAG, "ROLE_PROPERTIES_ID");
 			qb.setTables(Tables.ROLE_PROPERTIES);
 			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
 			qb.appendWhere(BaseColumns._ID + "=" + ssid);
-			cursor = qb.query(db, projection, selection, selectionArgs, null,
-					null, sortOrder);			
+			cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 			break;
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
@@ -284,34 +260,53 @@ public class MafiaHelperProvider extends ContentProvider {
 		cursor.setNotificationUri(getContext().getContentResolver(), uri);
 		return cursor;
 
-
 	}
 
 	@Override
 	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+		int updateCount = 0;
+		String ssid;
+		switch (sUriMatcher.match(uri)) {
+		case PLAYERS:
+			updateCount = db.update(Tables.PLAYERS, values, selection, selectionArgs);
+			break;
+		case PLAYERS_ID:
+			ssid = uri.getPathSegments().get(Tables.SSID_PATH_POSITION);
+			String where = BaseColumns._ID + " = " + ssid;
+			if (!TextUtils.isEmpty(selection)) {
+				where += " AND " + selection;
+			}
+			updateCount = db.update(Tables.PLAYERS, values, where, selectionArgs);
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported URI: " + uri);
+		}
+		if (updateCount > 0) {
+			getContext().getContentResolver().notifyChange(uri, null);
+		}
+		return updateCount;
 	}
 
 	private static UriMatcher buildUriMatcher() {
-        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
-        final String authority = MafiaHelperTables.CONTENT_AUTHORITY;
+		final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+		final String authority = MafiaHelperTables.CONTENT_AUTHORITY;
 
-        matcher.addURI(authority, "players", PLAYERS);
-        matcher.addURI(authority, "players/*", PLAYERS_ID);
-        
-        matcher.addURI(authority, "player_effects", PLAYER_EFFECTS);
-        matcher.addURI(authority, "player_effects/*", PLAYER_EFFECTS_ID);
-        
-        matcher.addURI(authority, "player_history", PLAYER_HISTORY);
-        matcher.addURI(authority, "player_history/*", PLAYER_HISTORY_ID);
-        
-        matcher.addURI(authority, "roles", ROLES);
-        matcher.addURI(authority, "roles/*", ROLES_ID);
-        
-        matcher.addURI(authority, "role_properties", ROLE_PROPERTIES);
-        matcher.addURI(authority, "role_properties/*", ROLE_PROPERTIES_ID);
-        
-        return matcher;
-    } 
+		matcher.addURI(authority, "players", PLAYERS);
+		matcher.addURI(authority, "players/*", PLAYERS_ID);
+
+		matcher.addURI(authority, "player_effects", PLAYER_EFFECTS);
+		matcher.addURI(authority, "player_effects/*", PLAYER_EFFECTS_ID);
+
+		matcher.addURI(authority, "player_history", PLAYER_HISTORY);
+		matcher.addURI(authority, "player_history/*", PLAYER_HISTORY_ID);
+
+		matcher.addURI(authority, "roles", ROLES);
+		matcher.addURI(authority, "roles/*", ROLES_ID);
+
+		matcher.addURI(authority, "role_properties", ROLE_PROPERTIES);
+		matcher.addURI(authority, "role_properties/*", ROLE_PROPERTIES_ID);
+
+		return matcher;
+	}
 }
--- src/com/ridteam/mafiahelper/model/ContentProviderModel.java
+++ src/com/ridteam/mafiahelper/model/ContentProviderModel.java
@@ -3,6 +3,7 @@ package com.ridteam.mafiahelper.model;
 import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
+import android.provider.BaseColumns;
 import android.support.v4.content.CursorLoader;
 import android.util.Log;
 
@@ -24,58 +25,70 @@ public class ContentProviderModel implements IBaseModel {
 
 	public ContentProviderModel(Context context) {
 		super();
-
 		this.mContext = context;
 	}
 
 	public CursorLoader getPlayersLoader() {
 		Log.d(TAG, PlayersTable.CONTENT_URI.toString());
-		return new CursorLoader(mContext, PlayersTable.CONTENT_URI, null, null,
-				null, null);
+		return new CursorLoader(mContext, PlayersTable.CONTENT_URI, null, null, null, null);
 	}
 
 	@Override
 	public void addPlayer(String name) {
 		ContentValues contentValues = new ContentValues();
 		contentValues.put(PlayersTable.NAME, name);
-		mContext.getContentResolver().insert(PlayersTable.CONTENT_URI,
-				contentValues);
+		mContext.getContentResolver().insert(PlayersTable.CONTENT_URI, contentValues);
 	}
 
 	@Override
 	public void removePlayer(long playerId) {
-		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI,
-				String.valueOf(playerId));
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI, String.valueOf(playerId));
 		mContext.getContentResolver().delete(uri, null, null);
 	}
 
 	@Override
 	public void setRole(long playerId, long roleId) {
-		// TODO Auto-generated method stub
-
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI, String.valueOf(playerId));
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayersTable.ROLE_ID, roleId);
+		mContext.getContentResolver().update(uri, contentValues, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(playerId) });
 	}
 
 	@Override
-	public void setLive(long playerId, boolean isLive) {
-		// TODO Auto-generated method stub
-
+	public void setAlive(long playerId, boolean isAlive) {
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI, String.valueOf(playerId));
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayersTable.IS_ALIVE, isAlive);
+		mContext.getContentResolver().update(uri, contentValues, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(playerId) });
 	}
 
 	@Override
-	public void setMarkAsDead(long playerId, boolean isMarkAsDead) {
-		// TODO Auto-generated method stub
-
+	public void setMarkedAsDead(long playerId, boolean isMarkedAsDead) {
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI, String.valueOf(playerId));
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayersTable.IS_MARKED_AS_DEAD, isMarkedAsDead);
+		mContext.getContentResolver().update(uri, contentValues, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(playerId) });
 	}
 
 	@Override
-	public void killMarkAsDead() {
-		// TODO Auto-generated method stub
+	public void killMarkedAsDead() {
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayersTable.IS_ALIVE, 0);
+		mContext.getContentResolver().update(PlayersTable.CONTENT_URI, contentValues,
+				PlayersTable.IS_MARKED_AS_DEAD + "=?", new String[] { String.valueOf(1) });
 
 	}
 
 	@Override
 	public void setNomination(long playerId, boolean isNominant) {
-		// TODO Auto-generated method stub
+		Uri uri = Uri.withAppendedPath(PlayersTable.CONTENT_URI, String.valueOf(playerId));
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayersTable.IS_NOMINANT, isNominant);
+		mContext.getContentResolver().update(uri, contentValues, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(playerId) });
 
 	}
 
@@ -111,31 +124,31 @@ public class ContentProviderModel implements IBaseModel {
 
 	@Override
 	public CursorLoader getPlayersEffectsLoader(long playerId) {
-		// TODO Auto-generated method stub
-		return null;
+		Log.d(TAG, PlayerEffectsTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, PlayerEffectsTable.CONTENT_URI, null, PlayerEffectsColumns.PLAYER_ID + "=?",
+				new String[] { String.valueOf(playerId) }, null);
 	}
 
 	@Override
 	public void addPlayersEffects(long playerId, int type, int time) {
-		// TODO Auto-generated method stub
-
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(PlayerEffectsTable.TYPE, type);
+		contentValues.put(PlayerEffectsTable.TIME, time);
+		contentValues.put(PlayerEffectsTable.PLAYER_ID, playerId);
+		mContext.getContentResolver().insert(PlayersTable.CONTENT_URI, contentValues);
 	}
 
 	@Override
 	public void removePlayersEffects(long playerId, int type) {
-		mContext.getContentResolver()
-				.delete(PlayerEffectsTable.CONTENT_URI,
-						PlayerEffectsColumns.PLAYER_ID + "=?"
-								+ PlayerEffectsColumns.TYPE + "= ?",
-						new String[] { String.valueOf(playerId),
-								String.valueOf(type) });
+		mContext.getContentResolver().delete(PlayerEffectsTable.CONTENT_URI,
+				PlayerEffectsColumns.PLAYER_ID + "=?" + PlayerEffectsColumns.TYPE + "= ?",
+				new String[] { String.valueOf(playerId), String.valueOf(type) });
 
 	}
 
 	@Override
 	public void removePlayersEffects(long playerId) {
-		mContext.getContentResolver().delete(PlayerEffectsTable.CONTENT_URI,
-				PlayerEffectsColumns.PLAYER_ID + "=?",
+		mContext.getContentResolver().delete(PlayerEffectsTable.CONTENT_URI, PlayerEffectsColumns.PLAYER_ID + "=?",
 				new String[] { String.valueOf(playerId) });
 
 	}
@@ -147,81 +160,95 @@ public class ContentProviderModel implements IBaseModel {
 	}
 
 	@Override
-	public CursorLoader getPlayersHistoryLoader() {
-		// TODO Auto-generated method stub
-		return null;
+	public CursorLoader getPlayerHistoryLoader() {
+		Log.d(TAG, PlayerHistoryTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, PlayerHistoryTable.CONTENT_URI, null, null, null, null);
 	}
 
 	@Override
-	public CursorLoader getPlayersHistoryLoader(int limit) {
-		// TODO Auto-generated method stub
-		return null;
+	public CursorLoader getPlayerHistoryLoader(int limit) {
+		return new CursorLoader(mContext, PlayerHistoryTable.CONTENT_URI, null, null, null,
+				PlayerHistoryTable.LAST_GAME + " DESC LIMIT " + limit);
 	}
 
 	@Override
-	public void addPlayersHistory(String name, String date, boolean isWin) {
-		// TODO Auto-generated method stub
-
+	public void addPlayerHistory(String name, String date, boolean isWin) {
+		// ContentValues contentValues = new ContentValues();
+		// contentValues.put(PlayerHistoryTable.LAST_GAME, date);
+		// contentValues.put(PlayerHistoryTable.WINS, date);
+		// mContext.getContentResolver().insert(PlayersTable.CONTENT_URI,
+		// contentValues);
 	}
 
 	@Override
 	public void removePlayersHistory(long playerId) {
-		mContext.getContentResolver().delete(PlayerHistoryTable.CONTENT_URI,
-				PlayerHistoryTable.PLAYER_ID + "=?",
+		mContext.getContentResolver().delete(PlayerHistoryTable.CONTENT_URI, PlayerHistoryTable.PLAYER_ID + "=?",
 				new String[] { String.valueOf(playerId) });
 
 	}
 
 	@Override
 	public CursorLoader getRolesLoader() {
-		// TODO Auto-generated method stub
-		return null;
+		Log.d(TAG, RolesTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, RolesTable.CONTENT_URI, null, null, null, null);
 	}
 
 	@Override
 	public CursorLoader getRoleLoader(long roleId) {
-		// TODO Auto-generated method stub
-		return null;
+		Log.d(TAG, RolesTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, RolesTable.CONTENT_URI, null, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(roleId) }, null);
 	}
 
 	@Override
 	public void addRole(String name, String desc, int side, String picture) {
-		// TODO Auto-generated method stub
-
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(RolesTable.NAME, name);
+		contentValues.put(RolesTable.DESC, desc);
+		contentValues.put(RolesTable.SIDE, side);
+		contentValues.put(RolesTable.PICTURE, picture);
+		mContext.getContentResolver().insert(RolesTable.CONTENT_URI, contentValues);
 	}
 
 	@Override
-	public void editRole(long roleId, String name, String desc, int side,
-			String picture) {
-		// TODO Auto-generated method stub
-
+	public void editRole(long roleId, String name, String desc, int side, String picture) {
+		Uri uri = Uri.withAppendedPath(RolesTable.CONTENT_URI, String.valueOf(roleId));
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(RolesTable.NAME, name);
+		contentValues.put(RolesTable.DESC, desc);
+		contentValues.put(RolesTable.SIDE, side);
+		contentValues.put(RolesTable.PICTURE, picture);
+		;
+		mContext.getContentResolver().update(uri, contentValues, BaseColumns._ID + "=?",
+				new String[] { String.valueOf(roleId) });
 	}
 
 	@Override
 	public void removeRole(long roleId) {
-		Uri uri = Uri.withAppendedPath(RolesTable.CONTENT_URI,
-				String.valueOf(roleId));
+		Uri uri = Uri.withAppendedPath(RolesTable.CONTENT_URI, String.valueOf(roleId));
 		mContext.getContentResolver().delete(uri, null, null);
 	}
 
 	@Override
-	public CursorLoader getRolePropertysLoader(long roleId) {
-		// TODO Auto-generated method stub
-		return null;
+	public CursorLoader getRolePropertiesLoader(long roleId) {
+		Log.d(TAG, RolePropertiesTable.CONTENT_URI.toString());
+		return new CursorLoader(mContext, RolePropertiesTable.CONTENT_URI, null, RolePropertiesTable.ROLE_ID + "=?",
+				new String[] { String.valueOf(roleId) }, null);
 	}
 
 	@Override
 	public void addRoleProperty(long roleId, int type, int value) {
-		// TODO Auto-generated method stub
-
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(RolePropertiesTable.ROLE_ID, roleId);
+		contentValues.put(RolePropertiesTable.TYPE, type);
+		contentValues.put(RolePropertiesTable.VALUE, value);
+		mContext.getContentResolver().insert(RolesTable.CONTENT_URI, contentValues);
 	}
 
 	@Override
 	public void removeRolePropertysByRole(long roleId) {
-		mContext.getContentResolver().delete(RolePropertiesTable.CONTENT_URI,
-				RolePropertiesTable.ROLE_ID + "=?",
+		mContext.getContentResolver().delete(RolePropertiesTable.CONTENT_URI, RolePropertiesTable.ROLE_ID + "=?",
 				new String[] { String.valueOf(roleId) });
-
 	}
 
 }
